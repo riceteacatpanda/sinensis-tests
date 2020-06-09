e1895bf0ca875e36516bee8d79b8cdaced82e8a0
Tom <tom@tdpain.net>
2020-06-09 17:35:29 +0100

    More trailing slashes!

--- python-api/main.py
+++ python-api/main.py
@@ -17,7 +17,7 @@ dbConn = mysql.connector.connect(
 )
 
 
-@app.route("/jwt/generate")
+@app.route("/jwt/generate/")
 def generateJwt():
 
   enc_jwt = jwt.encode({"nbf": 1591698124, "foo":"bar"}, PRIVATE_KEY, algorithm="RS256")
@@ -27,7 +27,7 @@ def generateJwt():
   return resp
 
 
-@app.route("/jwt/read")
+@app.route("/jwt/read/")
 def readJwt():
   jwt_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYmYiOjE1OTE2OTgxMjR9.fphVmklaDmYxaBPAvrRKA8tFqumSt5s365llP0gY1jacbzFeH52TatL8ADXu5PuKHcVtHXKKlQ0Xldf3G6vB8XqlgsFrIxb1E2td64K8nufdxelNpF9GvSxBnA3lcqt4rOh1U2Bg54O2EoFr_kFR8QcTG6uhih_6nyYmlalxxWBUmzl796g2-B0pLCreXzqxAZGBbPDvoI4V5SclAwIg5n3zo3VOQPL87ztRkBRsQZjPIFoqeRc8tBvDYtpMbhZ7XaX-CmzZt4jQYNjWKbpn02QD6NAO0tmBPTC0D4s8uFLrRT65FYk625yLAu2wHWLaV4NeWz3Boent6O1-HVfLr8D7CTtmzucd_CsWb_Zyq5kplRhCC5-LPAQj0JH_WvDSn08lcQSZ37imU8tYEPiLGLB6-u5UA4y5HqzXgI1qqdQCU8dL0o64UyuiPZD4hkv3kSgc532jD0jQq24mRWxj9GUKH1Y0wSe263Y54UEkA0RbsunRFXepsdZATVc7u3s4PPrzAmIyr-TK1pGQtBKWXkqjD0hU2dCqW7eLj61nAe3tuzXQRh9iYeSyFZsicoK6VCbRNeAfm48c_FJOGTU5KOiDRnY441_fAlTQwNqFLoRcUm1paDm0xlrCO_B68j-FGdrbaqIgF-Io20aFFNj0UEOydiGXZ89tkImzisu4i14"
 
@@ -38,7 +38,7 @@ def readJwt():
   return resp
 
 
-@app.route("/sql/select")
+@app.route("/sql/select/")
 def selectSql():
   cursor = dbConn.cursor(prepared=True)
   cursor.execute("SELECT age FROM readtest WHERE id = %s", [1])
@@ -50,7 +50,7 @@ def selectSql():
   return resp
 
 
-@app.route("/sql/insert")
+@app.route("/sql/insert/")
 def insertSql():
   rand = random.randint(0, 100)
   
